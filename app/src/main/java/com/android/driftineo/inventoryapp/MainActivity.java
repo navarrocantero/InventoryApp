3cb64d08514d7f0fd14f21fb2cf5fcc80f098270
navarrocantero <navarrocantero@gmail.com>
2017-07-15 17:32:08 +0200

    added LoaderManager

--- app/src/main/java/com/android/driftineo/inventoryapp/MainActivity.java
+++ app/src/main/java/com/android/driftineo/inventoryapp/MainActivity.java
@@ -1,63 +1,35 @@
 package com.android.driftineo.inventoryapp;
 
+import android.app.LoaderManager;
 import android.content.ContentValues;
+import android.content.CursorLoader;
+import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.widget.ListView;
 
 import com.android.driftineo.inventoryapp.ProductContract.ProductEntry;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private static final int THIS_LOADER = 0;
+    ProductAdapter productAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayDatabaseInfo();
+
+        ListView listView = (ListView) findViewById(R.id.list_product);
+        productAdapter = new ProductAdapter(this, null);
+        listView.setAdapter(productAdapter);
         addNewProducts();
+        getLoaderManager().initLoader(THIS_LOADER,null,this);
 
     }
 
-    private void displayDatabaseInfo() {
-
-        String[] projection = {
-                ProductEntry._ID,
-                ProductEntry.COLUMN_PRODUCT_NAME,
-                ProductEntry.COLUMN_PRODUCT_QUANTITY,
-                ProductEntry.COLUMN_PRODUCT_PRICE};
-
-        Cursor cursor = getContentResolver().query(
-                ProductEntry.CONTENT_URI,
-                projection, null, null, null);
-
-        TextView displayView = (TextView) findViewById(R.id.productText);
-
-        try {
-            displayView.setText("The product table contains " + cursor.getCount()
-                    + " products. \n\n");
-            displayView.append(ProductEntry.COLUMN_PRODUCT_NAME + " -" +
-                    ProductEntry.COLUMN_PRODUCT_QUANTITY + " - " +
-                    ProductEntry.COLUMN_PRODUCT_PRICE + "\n");
-
-
-            int indexName = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_NAME);
-            int indexQuantity = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_QUANTITY);
-            int indexPrice = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_PRICE);
-
-            while (cursor.moveToNext()) {
-
-                String currentName = cursor.getString(indexName);
-                String currentQuantity = cursor.getString(indexQuantity);
-                String currentPrice = cursor.getString(indexPrice);
-                displayView.append(("\n" + currentName + " - " + currentQuantity + " - "
-                        + currentQuantity));
-            }
-        } finally {
-            cursor.close();
-        }
-    }
 
     private void addNewProducts() {
 
@@ -65,12 +37,41 @@ public class MainActivity extends AppCompatActivity {
         contentValues.put(ProductEntry.COLUMN_PRODUCT_NAME, "cola");
         contentValues.put(ProductEntry.COLUMN_PRODUCT_PRICE, "20");
         contentValues.put(ProductEntry.COLUMN_PRODUCT_QUANTITY, "20");
-        for (int i = 0; i <100 ; i++) {
+        for (int i = 0; i < 5; i++) {
 
             Uri newUri = getContentResolver().insert(ProductEntry.CONTENT_URI, contentValues);
         }
 
     }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+
+        String[] projection = {
+                ProductEntry._ID,
+                ProductEntry.COLUMN_PRODUCT_NAME,
+                ProductEntry.COLUMN_PRODUCT_QUANTITY,
+                ProductEntry.COLUMN_PRODUCT_PRICE};
+
+        return new CursorLoader(this,
+                ProductEntry.CONTENT_URI,
+                projection,
+                null,
+                null,
+                null);
+    }
+
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        productAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        productAdapter.swapCursor(null);
+    }
+
 
 }
 
---
+++ app/src/main/java/com/android/driftineo/inventoryapp/ProductAdapter.java
@@ -0,0 +1,42 @@
+package com.android.driftineo.inventoryapp;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by driftineo on 15/7/17.
+ */
+
+public class ProductAdapter extends CursorAdapter{
+    public ProductAdapter(Context context, Cursor c) {
+        super(context, c, 0);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+
+        TextView  productNameTextView = (TextView) view.findViewById(R.id.productName);
+        TextView  productQuantityTextView = (TextView) view.findViewById(R.id.productQuantity);
+        TextView  productPriceTextView = (TextView) view.findViewById(R.id.productPrice);
+
+        String  productName = cursor.getString(cursor.getColumnIndexOrThrow(ProductContract.ProductEntry.COLUMN_PRODUCT_NAME));
+        String  productQuantity = cursor.getString(cursor.getColumnIndexOrThrow(ProductContract.ProductEntry.COLUMN_PRODUCT_QUANTITY));
+        String  price = cursor.getString(cursor.getColumnIndexOrThrow(ProductContract.ProductEntry.COLUMN_PRODUCT_PRICE));
+
+        productNameTextView.setText(productName);
+        productQuantityTextView.setText(productQuantity);
+        productPriceTextView.setText(price);
+
+    }
+}
